fadec63518019e3e8855454bb5008cb74156454a
Pridon <fridontetradze@gmail.com>
2021-12-01 15:09:17 +0400

    quicknote and bash for compositor.sh

--- scripts/compositor.sh
+++ scripts/compositor.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 picom
 if [[ ${?} -ne 0 ]]
---
+++ scripts/quicknote.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+noteFilename="${HOME}/notes/note-$(date +%Y-%m-%d).md"
+
+if [ ! -f $noteFilename ]
+then
+  echo "# Notes for $(date +%Y-%m%d)" > $noteFilename
+fi
+
+nvim -c "norm Go" \
+  -c "norm Go## $(date +%H:%M)" \
+  -c "norm G2o" \
+  -c "norm zz" \
+  -c "startinsert" $noteFilename
